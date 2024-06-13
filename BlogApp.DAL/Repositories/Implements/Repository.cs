ea40519a1d170127fb3e1ef025c182c66dd2f05c
Brazencobra <elxan.memmedli2003@gmail.com>
2024-06-13 23:58:32 +0400

    CreateArchitectureCommit

---
+++ BlogApp.DAL/Repositories/Implements/Repository.cs
@@ -0,0 +1,27 @@
+﻿using BlogApp.Core.Entities.Commons;
+using BlogApp.DAL.Contexts;
+using BlogApp.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.DAL.Repositories.Implements
+{
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        readonly AppDbContext _context;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public DbSet<TEntity> Table => _context.Set<TEntity>();
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Table;
+        }
+    }
+}
---
+++ BlogApp.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.Core.Entities.Commons;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.DAL.Repositories.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity, new()
+{
+    public IQueryable<TEntity> GetAll();
+    public DbSet<TEntity> Table { get;}
+}
